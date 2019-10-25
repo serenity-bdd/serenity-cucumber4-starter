b5c67bc788638fd171d646691df13ecc3c1db846
Yakim H. Rachev <jakimcho@gmail.com>
2019-10-25 15:38:41 +0300

    Fixed the selectors for search field and search button

--- src/test/java/starter/search/SearchForm.java
+++ src/test/java/starter/search/SearchForm.java
@@ -3,6 +3,6 @@ package starter.search;
 import org.openqa.selenium.By;
 
 public class SearchForm {
-    public static By SEARCH_FIELD = By.cssSelector(".js-search-input");
-    public static By SEARCH_BUTTON = By.cssSelector(".js-search-button");
+    public static By SEARCH_FIELD = By.id("search_form_input_homepage");
+    public static By SEARCH_BUTTON = By.id("search_button_homepage");
 }
